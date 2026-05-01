17cad7c61bd3faf8e0c746473fb3cc59cc01975b
Mario Faillace <mariofaillace73@gmail.com>
2026-05-01 10:41:47 +0200

    created server.js, created startServer and books getter function

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/books", async (req, res, next)=>{
+    try{
+        const books = [];
+        res.json(books);
+    }catch(err){
+        next(err);
+    }
+});
+
+async function startServer(){
+    try{
+        app.listen(PORT, () =>{
+            console.log(`server running on http://localhost:${PORT}`);
+        });
+    }catch(err){
+        console.error("failed to start server. Code error:", err);
+        process.exit(1);
+    }
+}
+
+startServer();
