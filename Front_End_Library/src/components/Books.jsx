5edab72f84d6df7ca8fc2df79bf8ce3ac8fe9d6c
giuli <giuli@example.com>
2026-05-02 17:28:44 +0200

    created basic strcture of Books.jsx

---
+++ Front_End_Library/src/components/Books.jsx
@@ -0,0 +1,15 @@
+//import from react-bootstrap/;
+//import axios?
+import { useState } from 'react';
+import Book from './Book.jsx';
+
+function Books()
+{
+    return(
+        <section>
+
+        </section>
+    )
+}
+
+export default Books;
