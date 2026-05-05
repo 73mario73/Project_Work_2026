e15a8f1e9b33baea76da46934b540bc346edbd19
Mario Faillace <mariofaillace73@gmail.com>
2026-05-05 10:32:16 +0200

    In server.js, added search query filtering for specific Title or Author

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import connectDB from "./db.js";
+import cors from "cors";
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 let db;
 
@@ -12,6 +16,18 @@ app.get("/books", async (req, res, next)=>{
     try{
         const booksCollection= db.collection("Library");
         const filter={};
+
+        if(req.query.title){
+            const titles = req.query.title.split(",");
+
+            filter.title = {$in: titles.map((title) => new RegExp(`^${title}$`, "i"))};
+        }
+
+        if(req.query.author){
+            const authors = req.query.author.split(",");
+
+            filter.author = {$in: authors.map((author) => new RegExp(`^${author}$`, "i"))};
+        }
         
         const books = await booksCollection.find(filter).toArray();
         res.json(books);
