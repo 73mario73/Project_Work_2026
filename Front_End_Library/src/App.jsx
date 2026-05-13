06ac763a7c61da24749036a549b0be9341ab54d7
giuli <giuli@example.com>
2026-05-13 17:17:06 +0200

    Created research bar

--- Front_End_Library/src/App.jsx
+++ Front_End_Library/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
+import { Box, TextField } from '@mui/material';
 import Books from './components/Books.jsx';
 
 function App() {
-	const [count, setCount] = useState(0)
+	//const [count, setCount] = useState(0)
+	const [research, setResearch] = useState("");
 
+	function handleResearch(research) 
+	{
+		setResearch(research);
+		//vorremmo che appena metto nuovo carattere cerca cose possibili con quello, quindi forse questa funzione va fatta da un'altra parte
+	}
+	
 	return (
-			<Books/>
+		<div>
+			<Box
+				component="form"
+				sx={{ '& > :not(style)': { m: 1, width: '200', maxWidth: '60%' }}}
+				noValidate
+				autoComplete="off" 
+			>
+				<TextField 
+					fullWidth 
+					id="filled-basic" 
+					label="Hai un nome in mente?"  // oppure placeholder?
+					variant="standard" 
+					researchP={research}
+					onResearch={handleResearch}
+					color="success" 
+				/>
+			</Box>
+
+			<Books/> {/*searchQuery={research} */}
+		</div>
 	)
 }
-
-export default App
+export default App;
